de8b17b29b999c42dfe55b90a6d51e72a1131b93
yiptsunho <thyipac@connect.ust.hk>
2023-01-31 09:29:01 +0800

    start up the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import ReactWeather, { useOpenWeather } from 'react-open-weather';
+import React from 'react';
 
 function App() {
+
+  const { data, isLoading, errorMessage } = useOpenWeather({
+    key: '8fd07b5ad4688b6f021b6f838cd398b7',
+    lat: '1.2921',
+    lon: '36.8219',
+    lang: 'en',
+    unit: 'metric', // values are (metric, standard, imperial)
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
+      <ReactWeather
+        isLoading={isLoading}
+        errorMessage={errorMessage}
+        data={data}
+        lang="en"
+        locationLabel="Nairobi"
+        unitsLabels={{ temperature: "C", windSpeed: "Km/h" }}
+        showForecast
+      />
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +37,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
